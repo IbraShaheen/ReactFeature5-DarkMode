75eb2efd275bc1022fac28c2c749296b1556aa9c
IbraShaheen <engibraheem1995@gmail.com>
2021-06-03 17:22:51 +0300

    dark & light mode switching

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import CookieList from "./components/ProductList";
 import Home from "./components/Home";
 import { ThemeProvider } from "styled-components";
 
+import { useState } from "react";
+
 const theme = {
   light: {
     mainColor: "#242424", // main font color
@@ -22,11 +24,27 @@ const theme = {
 };
 
 function App() {
+const [mode, setMode] = useState(theme.light)
+
+// const lightMode = () => {setMode(theme.light)}
+
+// const darkMode = () => {setMode(theme.dark)
+
+const toSwitch = () => {
+  if(mode === theme.light) {setMode(theme.dark)}
+  else{ {setMode(theme.light)}
+}
+  //console.log(mode);
+}
+
+//localStorage.toSwitch
+//console.log(mode);
+
   return (
-    <ThemeProvider theme={theme.light}>
+    <ThemeProvider theme={mode}>
       <GlobalStyle />
-      <ThemeButton onClick={() => alert("I do nothing..")}>
-        Dark Theme
+      <ThemeButton onClick={toSwitch}>
+      {mode===theme.light ? "Dark mode":"Light mode"}
       </ThemeButton>
       <Home />
       <CookieList />
